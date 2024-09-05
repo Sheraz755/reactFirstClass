53dfb6fd915cd46201c465b4281de28a28b2f010
sheraz756 <sherazzafar148@gmail.com>
2024-09-05 11:08:49 +0500

    firstReactClass

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import FirstComponent from "./FirstComponent/FirstComponent";
+
+const App = () => {
+	// const add = (a, b) => {
+	// 	return a + b;
+	// };
+	// console.log(add(1, 2));
+	// console.log(add(10, 21));
+
+	return (
+		<div>
+			<FirstComponent />
+			{/* <FirstComponent />
+
+			<FirstComponent />
+
+			<FirstComponent /> */}
+		</div>
+	);
+};
+
+export default App;
---
+++ src/FirstComponent/FirstComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FirstComponent = () => {
+	return (
+		<div>
+			<div className='firstHeading'>hello shahzad</div>
+			<div>How are you</div>
+		</div>
+	);
+};
+
+export default FirstComponent;
